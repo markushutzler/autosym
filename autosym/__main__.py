40dfd6ddb5c9cd9a273633b9be1b9ae456cd3744
Markus Hutzler <markus.hutzler@me.com>
2016-06-22 23:49:20 +0100

    main: Add __main__ entry point
    Signed-off-by: Markus Hutzler <markus.hutzler@me.com>

---
+++ autosym/__main__.py
@@ -0,0 +1,13 @@
+from __future__ import absolute_import
+
+import os
+import sys
+
+if __package__ == '':
+    path = os.path.dirname(os.path.dirname(__file__))
+    sys.path.insert(0, path)
+
+import autosym
+
+if __name__ == '__main__':
+    sys.exit(autosym.autosym.main())
